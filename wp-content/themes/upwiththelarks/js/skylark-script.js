17289d0fd265d8e472a78462260d82be84a8171e
Russell Williams <russellwilliamscc@gmail.com>
2017-06-05 23:18:02 +0100

    splits tagline/contact details across several lines, refactors js

--- wp-content/themes/upwiththelarks/js/skylark-script.js
+++ wp-content/themes/upwiththelarks/js/skylark-script.js
@@ -1,96 +1,158 @@
   var screenWidth = jQuery(window).width()
   var currentPosition = 0
 
-
   jQuery( document ).ready(function() {
-    cloneEvent()
-    buildPages()
-    getInstagramImage()
-    colours()
-  // setViewportWidth()
+   addContactDetails()
+   cloneEvent()
+   buildPages()
+   getInstagramImage()
+   colours()
+ })
+
+
+// resize function that deletes and redraws the size of the post tiles
+
+jQuery(function(){
   jQuery(window).on("resize", function(event){
     jQuery('#new-main').remove();
     buildPages();
+    colours()
   });
 })
 
-  jQuery(function(){
-    jQuery('.site-title').bind('click', function(event){
 
-        jQuery('#viewport').stop().animate({
-          scrollLeft: (0)
-        }, 1000);
-        currentPosition = 0
-        console.log("currentPosition", currentPosition)
-      
-      event.preventDefault();
+// function to scroll the viewport back to the first page (the post tiles)
 
-    });
+jQuery(function(){
+  jQuery('.site-title').bind('click', function(event){
+    jQuery('#viewport').stop().animate({
+      scrollLeft: (0)
+    }, 1000);
+    currentPosition = 0
+    event.preventDefault();
   });
+});
+
+  // function that creates an animated scroll left or right through the pages inside the 'viewport' after a link is clicked in the header menu
 
   jQuery(function() {
-
     jQuery('ul>li>a').bind('click',function(event){
-
       var viewportPosition = jQuery('#viewport').offset().left;
       var anchor = jQuery(this);
       var targetAnchor = jQuery(anchor.attr('href')).offset().left
       var targetAnchorSum = targetAnchor - viewportPosition - 4
 
-      console.log("targetAnchor", targetAnchorSum)
       if(targetAnchor != viewportPosition + 4){
         var moveTo = targetAnchorSum + currentPosition
-        jQuery('#viewport').stop().animate({
+        jQuery('#viewport')
+        .stop()
+        .animate({
           scrollLeft: (moveTo)
         }, 1000);
         currentPosition = moveTo
-        console.log("currentPosition", currentPosition)
       }
       event.preventDefault();
     });
   });
 
+// takes the (hidden with css) tagline, generated within wordpress, splits at an asterix and separates onto new lines to create a multiple line tagline
 
-  function getInstagramImage(){
-    setTimeout(function(){  var instaImage = jQuery('div.post-wrapper').eq(1).find('img')
-      var instagramClone = jQuery('.instagram-clone')
-      instaImage.removeAttr("height width").css({"display" : "block", "height" : "100%", "width" : "100%"}).clone().appendTo(instagramClone) }, 2000);
+function addContactDetails(){
+  var split = jQuery('.site-description')[0].innerHTML.split('*')
+  jQuery("<div></div>")
+  .addClass('split-tagline')
+  .appendTo('.site-branding');
+
+  for (var i = 0; i < split.length; i++){
+    jQuery('<p>').html(split[i]).appendTo('.split-tagline')
+    console.log("bang bang")
   }
+};
 
-  function cloneEvent(){
-    var openingHoursContent = jQuery('div.hidden-opening-hours').find(".simcal-event-details").first()
-    var clonedOpeningHours= jQuery('.cloned-opening-hours')
-    openingHoursContent.clone().appendTo(clonedOpeningHours)
-    var eventContent = jQuery( "div.hidden-events").find(".simcal-event-details").first()
-    var clonedEvent = jQuery('.cloned-event')
-    eventContent.clone().appendTo(clonedEvent)
-  }
+// a plugin is used to pull the most recent image from instagram, this strips away all the irrelevant html and presents just the image without
+
+function getInstagramImage(){
+
+  setTimeout(function(){
+    var instagramClone = jQuery('.instagram-clone')
+    var instaImage = jQuery('div.post-wrapper')
+    .eq(1)
+    .find('img')
+
+    instaImage.removeAttr("height width")
+    .css({"display" : "block", "height" : "100%", "width" : "100%"})
+    .clone()
+    .appendTo(instagramClone) }, 2000);
+}
 
 
-  function buildPages(){
-    var newPosts = jQuery('.page-builder').eq(0)
-    jQuery("#main").clone().removeAttr('id').attr('id', "new-main").appendTo(newPosts)
-    var width = jQuery('#viewport').width();
-    jQuery('#viewport').height(width)
-    jQuery('.post-wrapper').css("width", (width - 10) / 3 )
-    var cw = jQuery('.post-wrapper').width();
-    jQuery('.post-wrapper').css({'height':cw+'px'});
 
-    var viewportWidth = jQuery('#viewport').width()
-    jQuery('.page-builder').css('width', (viewportWidth) +'px')
+function cloneEvent(){
 
-    var viewportHeight = jQuery('#viewport').height()
+  var openingHoursContent = jQuery('div.hidden-opening-hours')
+  .find(".simcal-event-details")
+  .first()
+  var eventContent = jQuery( "div.hidden-events")
+  .find(".simcal-event-details")
+  .first()
 
-    jQuery('.page-wrapper').css('height', (viewportHeight) +'px')
-    jQuery('.page-builder').css('height', (viewportHeight) +'px')
-  }
+  var clonedOpeningHours= jQuery('.cloned-opening-hours')
+  var clonedEvent = jQuery('.cloned-event')
 
-  function colours(){
-    var colour = ['pink', 'orange', 'grey', 'yellow', 'blue', 'white', 'green', 'purple']
+  openingHoursContent.clone()
+  .appendTo(clonedOpeningHours)
 
-    for (var i = 0; i < 9; i++){
-      var x = Math.floor((Math.random() * 9) + 1);
-      jQuery('.entry-content').eq(8+i).css('background', colour[x])
+  eventContent.clone()
+  .appendTo(clonedEvent)
+}
+
+
+function buildPages(){
+
+  var newPosts = jQuery('.page-builder').eq(0)
+  var viewportWidth = jQuery('#viewport').width()
+  var viewportHeight = jQuery('#viewport').height()
+  
+  var cw = jQuery('.post-wrapper')
+  .css("width", (viewportWidth - 10) / 3 )
+  .width();
+
+  jQuery("#main")
+  .clone()
+  .removeAttr('id')
+  .attr('id', "new-main")
+  .appendTo(newPosts)
+
+  jQuery('#viewport').height(viewportWidth)
+  // jQuery('.post-wrapper').css("width", (width - 10) / 3 )
+  jQuery('.post-wrapper').css({'height':cw+'px'});
+  jQuery('.page-builder').css('width', (viewportWidth) +'px')
+  jQuery('.page-wrapper').css('height', (viewportHeight) +'px')
+  jQuery('.page-builder').css('height', (viewportHeight) +'px')
+}
+
+// sets background colours of the tiles (make it randomly selected?)
+
+function colours(){
+  var colour = ['pink', 'grey', 'orange', 'yellow', 'white', 'turquoise', 'green', 'purple', 'grey']
+
+  for (var i = 0; i < 9; i++){
+      // var x = Math.floor((Math.random() * 9) + 1);
+      jQuery('.entry-content').eq(9+i).css('background', colour[i])
     }
   }
 
+
+  // function colours(){
+
+  //   for (var i = 0; i < 9; i++){
+
+  //     var a = Math.floor((Math.random() * (9 - 1)) + 1);
+  //     var b = Math.floor((Math.random() * (9 - 1)) + 1);
+  //     var c = Math.floor((Math.random() * (9 - 1)) + 1);
+  //     var d = Math.floor((Math.random() * (9 - 1)) + 1);
+  //     var e = Math.floor((Math.random() * (9 - 1)) + 1);
+  //     var f = Math.floor((Math.random() * (9 - 1)) + 1);
+  //     jQuery('.entry-content').eq(8+i).css('background', "#"+a+b+c+d+e+f)
+  //   }
+  // }
